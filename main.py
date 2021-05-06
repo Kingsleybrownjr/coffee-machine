3de284123710ee1489a69ffa67ba49605369e9c0
Kingsley Brown <kingsleybrownjr@yahoo.com>
2021-05-06 14:36:55 -0500

    added the finishing touches, such as functions and print statements

---
+++ main.py
@@ -0,0 +1,37 @@
+from game_data import MENU, resources
+
+profit = 0
+is_on = True
+
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingreedients are insufficient"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+        return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted"""
+    print('Please insert coins.')
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10
+    total += int(input("How many nickels?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
+
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == 'report':
+        print(f"Water : {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}")
+        print(f"Money: {profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
